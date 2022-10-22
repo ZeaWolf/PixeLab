20d402a1835c55272f16b78ba160421fc7ba829a
ShuguiCSLearner <shuguichen1@gmail.com>
2022-10-21 23:38:11 -0400

    resource-model changed Comments type

--- server/models/resource-model.js
+++ server/models/resource-model.js
@@ -2,6 +2,15 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 const ObjectId = Schema.Types.ObjectId
 
+// const CommentSchema = new Schema(
+//     {
+//         userName:       {type: String, required: true},
+//         comment:        {type: String, required: true},
+//         Date:           {type: Date, required: true}
+//     },
+//     {timestamps: true}
+// );
+
 const ResourceSchema = new Schema(
     {
         Type:           {type: String, required: true},
@@ -10,11 +19,13 @@ const ResourceSchema = new Schema(
         Author:         {type: String, required: true},
         Image:          {type: String, required: true},
         Like:           {type: Number, required: true},
-        Comments:       {type: [{String, String}], required: true},
+        Comments:       {type: [[String]], required: true},
         PublishTime:    {type: Date, required: true, default: Date.now()},
         Description:    {type: String, required: true}
     },
     {timestamps: true}
 );
 
+
 module.exports = mongoose.model("Resource", ResourceSchema);
+// module.exports = mongoose.model("Comment", CommentSchema);
