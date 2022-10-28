b7df9032040fdd26b5582c5bd8675c67c41be9ad
Shugui Chen <shuguichen1@gmail.com>
2022-10-28 17:17:58 -0400

    C: working on the AppBanner component

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';  // using the css
+import {React} from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+// ** functions not implemented
+// import { AuthContextProvider } from './auth';
+// import { GlobalStoreContextProvider } from './store'
+
+
+// ** components no implemented
+// const App = () => {
+//     return (
+//         <BrowserRouter>
+//             <AuthContextProvider>
+//                 <GlobalStoreContextProvider>              
+//                     <AppBanner />
+//                     <Switch>
+//                         <Route path="/" exact component={HomeWrapper} />
+//                         <Route path="/register/" exact component={RegisterScreen} />
+//                         <Route path="/login/" exact component={LoginScreen} />
+//                         <Route path="/top5list/:id" exact component={WorkspaceScreen} />
+//                     </Switch>
+//                 </GlobalStoreContextProvider>
+//             </AuthContextProvider>
+//         </BrowserRouter>
+//     )
+// }
+
+// export default App
---
+++ client/src/components/AppBanner.js
@@ -0,0 +1,28 @@
+import { useContext, useState } from 'react';
+import { Link } from 'react-router-dom'
+
+//Functions not implement yet
+// import AuthContext from '../auth';
+// import GlobalStoreContext from '../store'
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+
+export default function AppBanner(){
+    const [anchorEl, setAnchorEl] = useState(null);
+    const isMenuOpen = Boolean(anchorEl);
+
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+}
