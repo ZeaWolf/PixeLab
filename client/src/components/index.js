3ef946a92166c1a8cbb231142babe2ebf9801d86
Shugui Chen <shuguichen1@gmail.com>
2022-10-29 15:16:49 -0400

    C: src/componenests/index.js for export module

---
+++ client/src/components/index.js
@@ -0,0 +1,23 @@
+import AppBanner from './AppBanner'
+import CommunityScreen from './CommunityScreen'
+import ErrorModal from './ErrorModal'
+import HomeScreen from './HomeScreen'
+import LoginScreen from './LoginScreen'
+import MapScreen from './MapScreen'
+import RegisterScreen from './RegisterScreen'
+import ResourceScreen from './ResourceScreen'
+import TilesetScreen from './TilesetScreen'
+import WelcomeScreen from './WeclomeScreen'
+
+export{
+    AppBanner,
+    CommunityScreen,
+    ErrorModal,
+    HomeScreen,
+    LoginScreen,
+    MapScreen,
+    RegisterScreen,
+    ResourceScreen,
+    TilesetScreen,
+    WelcomeScreen
+}
