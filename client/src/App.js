8796071b8759ec254e1b8a8010bcc2368e2c89bf
ZeaWolf <chengzhi.dong66@gmail.com>
2022-10-30 19:11:55 -0400

    D: home temp for Tang

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ const App = () => {
             {/* <AuthContextProvider>
                 <GlobalStoreContextProvider>               */}
                     <AppBanner />
-                    <NavigationBar/>
+                    {/* <NavigationBar/> */}
                     <Switch>
                         <Route path="/" exact component={WelcomeScreen} />
                         <Route path="/register/" exact component={RegisterScreen} />
