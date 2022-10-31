92ae96a36611be21e515aff16b8ac41ae51a43d9
ZeaWolf <chengzhi.dong66@gmail.com>
2022-10-31 03:45:18 -0400

    D: home screen appearance done

--- client/src/components/HomeMapCard.js
+++ client/src/components/HomeMapCard.js
@@ -2,14 +2,17 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import CardActions from '@mui/material/CardActions';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { Box,CardHeader, CardActionArea, IconButton, Grid} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 export default function HomeMapCard(props) {
   const { ImgNamePair } = props;
 
   return (
-    <Card class="home-map-card">
+    <Card className="home-map-card">
       <CardActionArea>
         <CardMedia
           component="img"
@@ -18,9 +21,11 @@ export default function HomeMapCard(props) {
           alt="img"
         />
         <CardContent>
-          <Typography gutterBottom variant="h3" component="div">
-            {ImgNamePair.name}
-          </Typography>
+          <Box style={{ display: 'flex', flexDirection: 'row'}}>
+          <Typography sx={{ flexGrow: 1 }}>{ImgNamePair.name}</Typography>
+          <IconButton aria-label="rename"><EditIcon /></IconButton>
+          <IconButton aria-label="delete"><DeleteIcon /></IconButton>
+          </Box>
         </CardContent>
       </CardActionArea>
     </Card>
--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -6,6 +6,7 @@ import List from '@mui/material/List'
 import Grid from '@mui/material/Grid';
 import HomeMapCard from './HomeMapCard';
 import HomeTilesetCard from './HomeTilesetCard';
+import { styled } from '@mui/material/styles';
 //import DeletionModal from "./DeletionModal"
 //import NavigationBar from "./NavigationBar"
 //import Statusbar from "./Statusbar"
@@ -14,6 +15,18 @@ import HomeTilesetCard from './HomeTilesetCard';
 export default function HomeScreen() {
     //const { auth } = useContext(AuthContext);
     //const { store } = useContext(GlobalStoreContext);
+
+    const AddButton = styled(IconButton)({
+        'position': 'relative',
+        'backgroundColor': '#008CBA',
+        'borderWidth': '5px',
+        'color': 'white',
+        'marginLeft': '20%',
+        'marginTop': '15%',
+        'height': '60%',
+        'width': '60%',
+        'borderRadius': '12px',
+    })
 
     
 
@@ -29,33 +42,50 @@ export default function HomeScreen() {
             <Box>
                 <Grid container spacing={2}>
 
-                <Grid item xs={4} md={3}><IconButton class="home-add-button" ><AddIcon sx={{ fontSize: 140 }} /></IconButton></Grid>
-                <Grid item xs={6} md={9}>
+                <Grid item xs={4} md={2}>
+                    <AddButton  ><AddIcon sx={{ fontSize: 100 }} /></AddButton >
+                </Grid>
+                <Grid item xs={6} md={10}>
                 <List id="home-map-list" style={{ display: 'flex', flexDirection: 'row', padding: 0}}>
-                    <HomeMapCard/>
-                    <HomeMapCard/>
-                    <HomeMapCard/>
-                    <HomeMapCard/>
-                    <HomeMapCard/>
-                    <HomeMapCard/>
-                    <HomeMapCard/>
-                    <HomeMapCard/>
-                    <HomeMapCard/>
-                    <HomeMapCard/>
-                    <HomeMapCard/>
-                    <HomeMapCard/>
-                    <HomeMapCard/>
+                    <HomeMapCard ImgNamePair={{img:"/moutainforest.png", name:"Moutain Map"}}/>
+                    <HomeMapCard ImgNamePair={{img:"/rockland.jpeg", name:"Rockland Map"}}/>
+                    <HomeMapCard ImgNamePair={{img:"/moutainforest.png", name:"Moutain Map"}}/>
+                    <HomeMapCard ImgNamePair={{img:"/rockland.jpeg", name:"Rockland Map"}}/>
+                    <HomeMapCard ImgNamePair={{img:"/moutainforest.png", name:"Moutain Map"}}/>
+                    <HomeMapCard ImgNamePair={{img:"/rockland.jpeg", name:"Rockland Map"}}/>
+                    <HomeMapCard ImgNamePair={{img:"/moutainforest.png", name:"Moutain Map"}}/>
+                    <HomeMapCard ImgNamePair={{img:"/rockland.jpeg", name:"Rockland Map"}}/>
                 </List>
                 </Grid>
                 </Grid>
             </Box>
+
             <Box sx={{ backgroundColor: 'warning.light'}}>
             <Typography style={{color: 'black', fontSize: 20, fontStyle: 'italic', fontWeight: "bold"}}>
                 Tilesets
             </Typography>
             </Box>
             <Box>
-            <HomeTilesetCard/>
+            <Box>
+                <Grid container spacing={2}>
+
+                <Grid item xs={4} md={2}>
+                    <AddButton className="home-add-button" ><AddIcon sx={{ fontSize: 100 }} /></AddButton>
+                </Grid>
+                <Grid item xs={6} md={10}>
+                <List id="home-map-list" style={{ display: 'flex', flexDirection: 'row', padding: 0}}>
+                    <HomeTilesetCard ImgNamePair={{img:"/pikachu.jpeg", name:"Pikachu Tileset"}}/>
+                    <HomeTilesetCard ImgNamePair={{img:"/charmander.jpeg", name:"Charmander Tileset"}}/>
+                    <HomeTilesetCard ImgNamePair={{img:"/pikachu.jpeg", name:"Pikachu Tileset"}}/>
+                    <HomeTilesetCard ImgNamePair={{img:"/charmander.jpeg", name:"Charmander Tileset"}}/>
+                    <HomeTilesetCard ImgNamePair={{img:"/pikachu.jpeg", name:"Pikachu Tileset"}}/>
+                    <HomeTilesetCard ImgNamePair={{img:"/charmander.jpeg", name:"Charmander Tileset"}}/>
+                    <HomeTilesetCard ImgNamePair={{img:"/pikachu.jpeg", name:"Pikachu Tileset"}}/>
+                    <HomeTilesetCard ImgNamePair={{img:"/charmander.jpeg", name:"Charmander Tileset"}}/>
+                </List>
+                </Grid>
+                </Grid>
+            </Box>
             </Box>
         </div>
     )
