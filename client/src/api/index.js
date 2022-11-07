8184b73e3b961262eb20c4b87d90a39743719502
Nicerrika <runkai.qiu@stonybrook.edu>
2022-11-06 23:14:42 -0500

    Q:update

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 axios.defaults.withCredentials = true;
 const api = axios.create({
-    baseURL: ['https://sbucsepixelab.herokuapp.com/api', 'http://localhost:4000/api'],
+    baseURL: ['https://sbucsepixelab.herokuapp.com/api', 'http://localhost:4000/api' ],
 })
 
 // User part
