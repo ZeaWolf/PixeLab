f16a301323571dd012d627e14ae855eec72d8176
Nicerrika <runkai.qiu@stonybrook.edu>
2022-11-19 20:57:27 -0500

    Q:Update Map Editor

--- client/src/components/MapScreen.js
+++ client/src/components/MapScreen.js
@@ -1,36 +1,149 @@
 
 import { Fab, Typography, Box, Button } from '@mui/material';
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState,useRef } from 'react'
 import NavigationBar from './NavigationBar'
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AuthContext from '../auth'
-var canvas = document.querySelector("canvas");
 // var tilesetContainer = document.querySelector(".tileset-container");
 // var tilesetSelection = document.querySelector(".tileset-container_selection");
-// var tilesetImage = document.querySelector("#tileset-source");
 
 
 export default function MapScreen() {
+    var tilesetImage = document.querySelector("#tileset-source");
+    var tilesetSelection = document.querySelector(".tileset-container_selection");
+    var tilesetContainer = document.querySelector(".tileset-container");
+    let canvas = document.querySelector("canvas");
+    const { auth } = useContext(AuthContext);
+    var isMouseDown = false;
+    var currentLayer = 0;
+
+    var selection = [0, 0];
+
+    useEffect(() => {
+        // store.closeCurrentList();
+        console.log("FUCK")
+        console.log(tilesetImage)
+        // store.loadMaps();
+    }, []);
+
     const styles = {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: "rgb(235, 225, 225)"
     };
+    
+    if(tilesetImage){
+        var layers = [
+            //Bottom
+            {
+               //Structure is "x-y": ["tileset_x", "tileset_y"]
+               //EXAMPLE: "1-1": [3, 4],
+               "0-0": [0, 0],
+               "1-0": [1, 0],
+            },
+            //Middle
+            {},
+            //Top
+            {}
+         ];
 
-    const { auth } = useContext(AuthContext);
+         tilesetContainer.addEventListener("mousedown", (event) => {
+            selection = getCoords(event);
+            tilesetSelection.style.left = selection[0] * 32 + "px";
+            tilesetSelection.style.top = selection[1] * 32 + "px";
+         });
 
-    let mapPage = 
-        <div className='full-screen'>
-            <Typography style={{color: 'black', fontSize: 20, fontStyle: 'italic', fontWeight: "bold"}}>
-                401: Unauthorized Access
-            </Typography>
-        </div>
+         function addTile(mouseEvent) {
+            var clicked = getCoords(mouseEvent);
+            var key = clicked[0] + "-" + clicked[1];
+         
+            if (mouseEvent.shiftKey) {
+               delete layers[currentLayer][key];
+            } else {
+               layers[currentLayer][key] = [selection[0], selection[1]];
+            }
+            draw();
+         }
 
-    if(auth.loggedIn){
-        mapPage = 
+         canvas.addEventListener("mousedown", () => {
+            isMouseDown = true;
+         });
+         canvas.addEventListener("mouseup", () => {
+            isMouseDown = false;
+         });
+         canvas.addEventListener("mouseleave", () => {
+            isMouseDown = false;
+         });
+         canvas.addEventListener("mousedown", addTile);
+         canvas.addEventListener("mousemove", (event) => {
+            if (isMouseDown) {
+               addTile(event);
+            }
+         });
+
+        function getCoords(e) {
+            const { x, y } = e.target.getBoundingClientRect();
+            const mouseX = e.clientX - x;
+            const mouseY = e.clientY - y;
+            return [Math.floor(mouseX / 32), Math.floor(mouseY / 32)];
+         }
+
+        function draw() {
+            console.log(canvas);
+            var ctx = canvas.getContext("2d");
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+         
+            var size_of_crop = 32;
+            
+            layers.forEach((layer) => {
+               Object.keys(layer).forEach((key) => {
+                  //Determine x/y position of this placement from key ("3-4" -> x=3, y=4)
+                  var positionX = Number(key.split("-")[0]);
+                  var positionY = Number(key.split("-")[1]);
+                  var [tilesheetX, tilesheetY] = layer[key];
+         
+                  ctx.drawImage(
+                     tilesetImage,
+                     tilesheetX * 32,
+                     tilesheetY * 32,
+                     size_of_crop,
+                     size_of_crop,
+                     positionX * 32,
+                     positionY * 32,
+                     size_of_crop,
+                     size_of_crop
+                  );
+               });
+            });
+         }
+
+        tilesetImage.onload = function() {
+        // layers = defaultState;
+            draw();
+        // setLayer(0);
+        }
+
+        tilesetImage.src = "https://assets.codepen.io/21542/TileEditorSpritesheet.2x_2.png"
+    }
+
+    // tilesetImage.onload = function() {
+    //     // layers = defaultState;
+    //     draw();
+    //     // setLayer(0);
+    // }
+
+    // let mapPage = 
+    //     <div className='full-screen'>
+    //         <Typography style={{color: 'black', fontSize: 20, fontStyle: 'italic', fontWeight: "bold"}}>
+    //             401: Unauthorized Access
+    //         </Typography>
+    //     </div>
+
+    // if(auth.loggedIn){
+    const  mapPage = 
         <div className='full-screen'>
             <NavigationBar/>
             <div className='right-screen'>
@@ -63,7 +176,8 @@ export default function MapScreen() {
                                 <aside>
                                     <label style={{color: "black"}}>Tileset: </label>
                                     <div className="tileset-container">
-                                        <img height="400" src="https://assets.codepen.io/21542/TileEditorSpritesheet.2x_2.png" />
+                                        <img id="tileset-source"/>
+                                        <div class="tileset-container_selection"></div>
                                     </div>
                                 </aside>
                             </div>
@@ -72,7 +186,22 @@ export default function MapScreen() {
                 </div>
             </div>
         </div>
-    }
+
+    // tilesetImage.onload = function() {
+    //     // layers = defaultState;
+    //     draw();
+    //     // setLayer(0);
+    // }
+    // }
+    // if(!auth.loggedIn){
+    //     // mapPage = 
+    //     // <div className='full-screen'>
+    //     //     <Typography style={{color: 'black', fontSize: 20, fontStyle: 'italic', fontWeight: "bold"}}>
+    //     //         401: Unauthorized Access
+    //     //     </Typography>
+    //     // </div>
+    // }
+
     return (
         <div className='outer-screen'>
             {mapPage}
